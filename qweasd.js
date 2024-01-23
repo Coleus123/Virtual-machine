02aaac0dfdfb7c8c82752b776198312266ae349a
Coleus123 <146657138+Coleus123@users.noreply.github.com>
2024-01-24 04:55:00 +0500

    Update qweasd.js

--- qweasd.js
+++ qweasd.js
@@ -46,62 +46,22 @@ while (mem[ip] != "exit"){
 			ip += 3;
 			break;
 		case 'loop':
-			if (mem[parseInt(mem[ip+1])] > 0){
+			if (stack[stack.lentgh -1] != ip || stack.lentgh == 0){
 				stack.push(ip);
-				ip+=2;
-				while (mem[ip] != "endloop"){
-					switch (mem[ip]) {
-						case 'input':
-							mem[parseInt(mem[ip+1])] = readlineSync.question('Введите число: ');
-							ip+=2;
-							break;
-						case 'set':
-							mem[parseInt(mem[ip+1])] = parseInt(mem[ip+2]);
-
-							ip+=3;
-							break;
-						case 'output':
-							console.log(mem[mem[ip+1]]);
-							ip+=2;
-							break;
-						case 'add':
-							mem[parseInt(mem[ip+3])] = mem[parseInt(mem[ip+1])] + mem[parseInt(mem[ip+2])];
-
-							ip +=4;
-							break;
-						case 'divc':
-							mem[parseInt(mem[ip+3])] = mem[parseInt(mem[ip+1])] % mem[parseInt(mem[ip+2])];
-							ip +=4;
-							break;
-						case 'div':
-							mem[parseInt(mem[ip+3])] = mem[parseInt(mem[ip+1])] / mem[parseInt(mem[ip+2])];
-							ip +=4;
-							break;
-						case 'mult':
-							mem[parseInt(mem[ip+3])] = mem[parseInt(mem[ip+1])] * mem[parseInt(mem[ip+2])];
-							ip +=4;
-							break;
-						case 'sub':
-							mem[parseInt(mem[ip+3])] = mem[parseInt(mem[ip+1])] - mem[parseInt(mem[ip+2])];
-							ip +=4;
-							break;
-						case 'assignment':
-							mem[parseInt(mem[ip+1])] = mem[parseInt(mem[ip+2])];
-							ip += 3;
-							break;
-					}
-				}
-				ip = stack[stack.length - 1];
-				break;
 			}
 			if (mem[parseInt(mem[ip+1])] == 0){
-				ip+=2;
 				while (mem[ip] != "endloop"){
-					ip += 1;
+					ip+=1;
 				}
 				stack.pop();
-				ip += 1;
+				ip+=1
+				
 				break;
 			}
+			ip+=2;
+			break;
+		case 'endloop':
+			ip = stack[stack.length - 1];
+			break;
 	}
 }
